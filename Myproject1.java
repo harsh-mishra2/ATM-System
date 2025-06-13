02fec0f1830d237fe19d1194772c88688ba9bcf0
harsh-mishra2 <155904202+harsh-mishra2@users.noreply.github.com>
2025-06-13 11:14:04 +0530

    Add files via upload

---
+++ Myproject1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package myproject1;
+
+/**
+ *
+ * @author Dell
+ */
+public class Myproject1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    mainpaga m1=new mainpaga();
+    m1.show();
+    }
+    
+}
